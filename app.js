505ff76b2301d9b74de6ed934159ebcb5c099f5a
Kellerman Rivero <krsloco@gmail.com>
2017-06-22 21:57:08 +0000

    Fixed cors

--- app.js
+++ app.js
@@ -11,16 +11,21 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(cors());
+//app.use(cors());
 
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-app.options('/', cors());
-app.get('/', routes.getRenderPage);
-app.post('/', routes.postRenderFile);
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.options('/', cors(corsOptions));
+app.get('/', cors(corsOptions), routes.getRenderPage);
+app.post('/', cors(corsOptions), routes.postRenderFile);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
