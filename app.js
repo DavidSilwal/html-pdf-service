f122a0cd68af74ba4d75407bf65b1412e6bbaa2a
Kellerman Rivero <krsloco@gmail.com>
2017-06-22 18:26:49 +0000

    Added cors

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 ﻿var express = require('express');
 var routes = require('./routes/main');
 var http = require('http');
-
 var app = express();
+var cors = require('cors');
 
 // all environments
 app.set('port', 8080);
@@ -11,12 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(cors());
 
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
+app.options('/', cors());
 app.get('/', routes.getRenderPage);
 app.post('/', routes.postRenderFile);
 
